5b1dc9c60b1604a291c3d40c9189f4473319dacd
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-06-08 20:14:19 -0400

    not finished

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,7 +1,9 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
+  include JSON
   base_uri 'https://www.bloc.io/api/v1'
 
 
@@ -16,9 +18,11 @@ class Kele
 
     raise 'Invalid credentials' if response.code == 401
 
-    p response
-
     @auth_token = response['auth_token']
+  end
 
+    def get_me
+    response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
+    JSON.parse(response)
   end
 end
