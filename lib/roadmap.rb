a5922fb4e72a658166908a3f8da788fa44f78cfa
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-06-10 14:55:00 -0400

    Can't include module

---
+++ lib/roadmap.rb
@@ -0,0 +1,12 @@
+module Roadmap
+
+  def get_roadmap(id)
+    response = self.class.get('/roadmaps/6660', headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
+  def get_checkpoint(id)
+    response = self.class.get('/checkpoints/2265', headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+end
