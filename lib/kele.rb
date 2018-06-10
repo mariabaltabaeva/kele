98ab0aee9e0219507bffac90f7c5b72cdd1877a7
maria.baltabaeva@gmail.com <maria.baltabaeva@gmail.com>
2018-06-10 13:49:49 -0400

    US-3:completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -25,4 +25,9 @@ class Kele
     response = self.class.get('/users/me', headers: { "authorization" => @auth_token })
     JSON.parse(response.body)
   end
+
+  def get_mentor_availability(id)
+   response = self.class.get('/mentors/529277/student_availability', headers: { "authorization" => @auth_token })
+   JSON.parse(response.body)
+  end
 end
